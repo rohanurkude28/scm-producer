75adb8dbf8aba46a35d9f689891d976092a30b7c
Rohan Urkude <rohan.urkude@here.com>
2022-03-02 21:11:39 +0530

    Implement Mapstruct with Lombok

--- src/main/java/com/spring/cloud/scmproducer/ScmProducerApplication.java
+++ src/main/java/com/spring/cloud/scmproducer/ScmProducerApplication.java
@@ -3,7 +3,7 @@ package com.spring.cloud.scmproducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //@Configuration, @EnableAutoConfiguration and @ComponentScan
 public class ScmProducerApplication {
 
 	public static void main(String[] args) {
