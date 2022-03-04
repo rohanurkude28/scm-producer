a82e828150edbed7779d8d29149c051439f32d01
Rohan Urkude <rohan.urkude@here.com>
2022-03-04 21:55:04 +0530

    Add Json Creator

--- src/main/java/com/spring/cloud/scmproducer/web/model/ItemPagedList.java
+++ src/main/java/com/spring/cloud/scmproducer/web/model/ItemPagedList.java
@@ -1,7 +1,11 @@
 package com.spring.cloud.scmproducer.web.model;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -13,5 +17,19 @@ public class ItemPagedList extends PageImpl<ItemDTO> {
 
     public ItemPagedList(List<ItemDTO> content) {
         super(content);
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public ItemPagedList(@JsonProperty("content") List<ItemDTO> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, size), totalElements);
     }
 }
